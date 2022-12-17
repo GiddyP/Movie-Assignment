b47199ae3ab222d48460e88db6b845313e82770d
giddyPwild <giddygbenga4@gmail.com>
2022-12-17 13:09:47 +0100

    added dist

--- src/assets/index.js
+++
@@ -1,9 +0,0 @@
-
-import logo from './logo.svg'
-
-export {
-    logo,
-};
-
-// This images are exported to the index.js file for
-// them to be processed at once
--- src/style.js
+++
@@ -1,7 +0,0 @@
-const styles = {
-  flexCenter: "flex justify-center items-center",
-  flexStart: "flex justify-center items-start",
-};
-
-
-export default styles;
